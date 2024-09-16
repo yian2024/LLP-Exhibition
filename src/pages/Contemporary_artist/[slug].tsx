e14d2340bd04ee6554a4ed4d1efe5438abf982e4
Yian <yianwang2024@github.com>
2024-09-15 23:30:29 -0500

    Reorganized the layout of artists page and homepage

--- src/pages/Contemporary_artist/[slug].tsx
+++ src/pages/Contemporary_artist/[slug].tsx
@@ -1,20 +1,22 @@
+import { getMarkdownContent, getMarkdownPaths } from "@lib/contentHelpers";
+
 import CanopyMDXRemote from "@src/components/MDX";
 import { FrontMatterPageProps } from "@customTypes/content";
+import { GetStaticPropsContext } from "next";
 import LayoutsBasicSidebar from "@src/components/Layouts/BasicSidebar";
 import { buildContentSEO } from "@src/lib/seo";
-import { getMarkdownContent } from "@lib/contentHelpers";
 
 /**
  * Specifies the relative path of the directory containing the Markdown.
  * This path is relative to the [root]/content/ directory. Modify this value
- * to change the directory that is used to generate the page.
+ * to change the directory that is used to generate pages.
  */
-const CONTENT_DIRECTORY = "southAfrica";
+const CONTENT_DIRECTORY = "Contemporary_artist";
 
 /**
  * This is a page that is generated from a Markdown file.
  */
-const ContentPage = ({ source, frontMatter }: FrontMatterPageProps) => {
+const ContentPageSlug = ({ source, frontMatter }: FrontMatterPageProps) => {
   return (
     <LayoutsBasicSidebar frontMatter={frontMatter}>
       <CanopyMDXRemote source={source} />
@@ -23,15 +25,28 @@ const ContentPage = ({ source, frontMatter }: FrontMatterPageProps) => {
 };
 
 /**
+ * This function is called at build time by Next.js. It specifies the paths
+ * that should be generated into HTML at build time. It is used to generate
+ * the static pages that are generated from Markdown files.
+ */
+export async function getStaticPaths() {
+  return {
+    paths: getMarkdownPaths(CONTENT_DIRECTORY),
+    fallback: false,
+  };
+}
+
+/**
  * This function is called at build time by Next.js. It specifies the props
  * that should be passed to the page component. It is used to pass the props
  * that are used to render the static pages that are generated from Markdown
  * files.
  */
-export async function getStaticProps() {
-  const path = `/${CONTENT_DIRECTORY}`;
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const slug = params?.slug as string;
+  const path = `/${CONTENT_DIRECTORY}/${slug}`;
   const { frontMatter, source } = await getMarkdownContent({
-    slug: "index",
+    slug,
     directory: CONTENT_DIRECTORY,
   });
   const seo = buildContentSEO(frontMatter, path);
@@ -41,4 +56,4 @@ export async function getStaticProps() {
   };
 }
 
-export default ContentPage;
+export default ContentPageSlug;
--- src/pages/Contemporary_artist/index.tsx
+++ src/pages/Contemporary_artist/index.tsx
@@ -1,22 +1,20 @@
-import { getMarkdownContent, getMarkdownPaths } from "@lib/contentHelpers";
-
 import CanopyMDXRemote from "@src/components/MDX";
 import { FrontMatterPageProps } from "@customTypes/content";
-import { GetStaticPropsContext } from "next";
 import LayoutsBasicSidebar from "@src/components/Layouts/BasicSidebar";
 import { buildContentSEO } from "@src/lib/seo";
+import { getMarkdownContent } from "@lib/contentHelpers";
 
 /**
  * Specifies the relative path of the directory containing the Markdown.
  * This path is relative to the [root]/content/ directory. Modify this value
- * to change the directory that is used to generate pages.
+ * to change the directory that is used to generate the page.
  */
-const CONTENT_DIRECTORY = "southAfrica";
+const CONTENT_DIRECTORY = "Contemporary_artist";
 
 /**
  * This is a page that is generated from a Markdown file.
  */
-const ContentPageSlug = ({ source, frontMatter }: FrontMatterPageProps) => {
+const ContentPage = ({ source, frontMatter }: FrontMatterPageProps) => {
   return (
     <LayoutsBasicSidebar frontMatter={frontMatter}>
       <CanopyMDXRemote source={source} />
@@ -25,28 +23,15 @@ const ContentPageSlug = ({ source, frontMatter }: FrontMatterPageProps) => {
 };
 
 /**
- * This function is called at build time by Next.js. It specifies the paths
- * that should be generated into HTML at build time. It is used to generate
- * the static pages that are generated from Markdown files.
- */
-export async function getStaticPaths() {
-  return {
-    paths: getMarkdownPaths(CONTENT_DIRECTORY),
-    fallback: false,
-  };
-}
-
-/**
  * This function is called at build time by Next.js. It specifies the props
  * that should be passed to the page component. It is used to pass the props
  * that are used to render the static pages that are generated from Markdown
  * files.
  */
-export async function getStaticProps({ params }: GetStaticPropsContext) {
-  const slug = params?.slug as string;
-  const path = `/${CONTENT_DIRECTORY}/${slug}`;
+export async function getStaticProps() {
+  const path = `/${CONTENT_DIRECTORY}`;
   const { frontMatter, source } = await getMarkdownContent({
-    slug,
+    slug: "index",
     directory: CONTENT_DIRECTORY,
   });
   const seo = buildContentSEO(frontMatter, path);
@@ -56,4 +41,4 @@ export async function getStaticProps({ params }: GetStaticPropsContext) {
   };
 }
 
-export default ContentPageSlug;
+export default ContentPage;
